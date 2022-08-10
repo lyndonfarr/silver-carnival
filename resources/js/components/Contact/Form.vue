f2ec1c775c8dc98524f08f4f0d9cd69ee368086a
Lyndon Farr <lyndonfarr@protonmail.com>
2022-08-10 19:43:53 -0300

    components

---
+++ resources/js/components/Contact/Form.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            bob: 0,
+        }
+    },
+    methods: {
+        updateBob() {
+            this.bob += 1;
+        },
+    },
+}
+</script>
