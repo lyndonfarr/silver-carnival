f1b2a0bb11d11324fc5bf33174bc932fa2029f72
Lyndon Farr <lyndonfarr@protonmail.com>
2022-08-11 07:08:15 -0300

    use new input components

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -6,75 +6,41 @@
             <div class="card-header">Create Contact</div>
             <div class="card-body">
                 <div class="form-row">
-                    <div class="form-group col-md-4">
-                        <label for="first_name">First Name</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="first_name"
-                            name="first_name"
-                            placeholder="First Name"
-                        >
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="middle_names">Middle Names</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="middle_names"
-                            name="middle_names"
-                            placeholder="Middle Names"
-                        >
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="last_name">Last Name</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="last_name"
-                            name="last_name"
-                            placeholder="Last Name"
-                        >
-                    </div>
+                    <text-input
+                        class="col-md-4"
+                        label="First Name"
+                        name="first_name"
+                    ></text-input>
+                    <text-input
+                        class="col-md-4"
+                        label="Middle Names"
+                        name="middle_names"
+                    ></text-input>
+                    <text-input
+                        class="col-md-4"
+                        label="Last Name"
+                        name="last_name"
+                    ></text-input>
                 </div>
                 <div class="form-row">
-                    <div class="form-group col-md-5">
-                        <label for="description">Description</label>
-                        <textarea
-                            class="form-control"
-                            id="description"
-                            name="description"
-                            placeholder="Description"
-                        ></textarea>
-                    </div>
-                    <div class="form-group col-md-7">
-                        <label for="notes">Notes</label>
-                        <textarea
-                            class="form-control"
-                            id="notes"
-                            name="notes"
-                            placeholder="Notes"
-                        ></textarea>
-                    </div>
+                    <textarea-input
+                        class="col-md-5"
+                        name="description"
+                        label="Description"
+                    ></textarea-input>
+                    <textarea-input
+                        class="col-md-7"
+                        name="notes"
+                        label="Notes"
+                    ></textarea-input>
                 </div>
                 <div class="form-row">
-                    <div class="form-group col-md-4">
-                        <label for="nationality">Nationality</label>
-                        <select id="nationality" name="nationality" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="nickname">Nickname</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="nickname"
-                            name="nickname"
-                            placeholder="Nickname"
-                        >
-                    </div>
+                    <nationality-input></nationality-input>
+                    <text-input
+                        class="col-md-4"
+                        name="nickname"
+                        label="Nickname"
+                    ></text-input>
                     <div class="form-group col-md-4">
                         DOB
                     </div>
