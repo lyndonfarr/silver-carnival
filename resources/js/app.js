98fef8240065fae8cab59c98b72075c16b85bee6
Lyndon Farr <lyndonfarr@protonmail.com>
2022-08-29 20:31:52 -0300

    register

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,22 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import Multiselect from 'vue-multiselect';
 
 import CountryInput from './components/Form/CountryInput.vue';
 import DateInput from './components/Form/DateInput.vue';
 import TextareaInput from './components/Form/TextareaInput.vue';
 import TextInput from './components/Form/TextInput.vue';
+
+import ValueStorage from './components/Utilities/ValueStorage.vue';
+import Vue from 'vue';
+
+
+Vue.component('multiselect', Multiselect);
+
 
 const app = new Vue({
     el: '#app',
@@ -19,5 +27,6 @@ const app = new Vue({
         DateInput,
         TextareaInput,
         TextInput,
+        ValueStorage,
     },
 });
