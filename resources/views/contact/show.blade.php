710e9d0f54b9ffaa4a2bc1c13aa5e95f783f8d42
Lyndon Farr <lyndonfarr@protonmail.com>
2022-09-25 18:07:50 -0300

    fill out Contact:show blade file

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -3,17 +3,47 @@
 @section('contactContent')
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
-        <span>{{ $contact->full_name }}</span>
-        <ul class="nav nav-pills card-header-pills">
+        <ul class="nav nav-pills card-header-pills d-flex">
+            @if (isset($contact->primaryInstagram))
             <li class="nav-item">
-                <edit-button
-                    route="{{ route('contacts.edit', $contact->id) }}"
-                ></edit-button>
+                <instagram-button instagram="{{ $contact->primaryInstagram->value }}"></instagram-button>
+            </li>
+            @endif
+            @if (isset($contact->primaryPhoneNumber))
+            <li class="nav-item">
+                <whats-app-button phone-number="{{ $contact->primaryPhoneNumber->value }}"></whats-app-button>
+            </li>
+            @endif
+        </ul>
+        <ul class="nav nav-pills card-header-pills d-flex">
+            <li class="nav-item ml-auto">
+                <edit-button route="{{ route('contacts.edit', $contact->id) }}"></edit-button>
             </li>
         </ul>
     </div>
-    <div class="card-body">
-
-    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            Name: {{ $contact->full_name }}
+        </li>
+        @if (isset($contact->nickname))
+        <li class="list-group-item">
+            AKA: {{ $contact->nickname }}
+        </li>
+        @endif
+        @if (isset($contact->dob))
+        <li class="list-group-item">
+            Age: {{ $contact->age }}
+        </li>
+        @endif
+        <li class="list-group-item">
+            Born: {{ $contact->dob }}
+        </li>
+        <li class="list-group-item">
+            Nationality: {{ $contact->nationality }}
+        </li>
+        <li class="list-group-item">
+            Notes: {{ $contact->notes }}
+        </li>
+    </ul>
 </div>
 @endsection
