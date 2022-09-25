6c089eb2cca04530b5ab5c9cb49eab196aa9f644
Lyndon Farr <lyndonfarr@protonmail.com>
2022-09-25 16:17:16 -0300

    full_name accessor and mutator, comments to separate out different sections of model

--- app/Contact.php
+++ app/Contact.php
@@ -10,12 +10,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Contact extends Model
 {
     /** 
+     * The editable model attributes
+     * 
      * @var array
      */
     protected $fillable = [
         'description',
         'dob',
         'first_name',
+        'full_name',
         'last_name',
         'middle_names',
         'nationality',
@@ -23,6 +26,16 @@ class Contact extends Model
         'notes',
     ];
 
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['full_name'];
+
+    //=======
+    //<RELATIONSHIPS>
+    //=======
     /**
      * a Contact belongsToMany Addresses
      * 
@@ -55,4 +68,47 @@ class Contact extends Model
             'type' => 'phone',
         ]);
     }
+    //=======
+    //</RELATIONSHIPS>
+    //=======
+
+    //=======
+    //<MUTATORS>
+    //=======
+    public function setFullNameAttribute($full_name): void
+    {
+        $full_name = trim($full_name);
+        $full_name = explode(' ', $full_name);
+        
+        $this->attributes['first_name'] = array_shift($full_name);
+
+        if (count($full_name)) {
+            $this->attributes['last_name'] = array_pop($full_name);
+        }
+
+        if (count($full_name)) {
+            $this->attributes['middle_names'] = join($full_name, ' ');
+        }
+    }
+    //=======
+    //</MUTATORS>
+    //=======
+
+    //=======
+    //<ACCESSORS>
+    //=======
+    /**
+     * Concat first_name, middle_names, and last_name column into one string
+     * 
+     * @return string
+     */
+    public function getFullNameAttribute(): string
+    {
+        return collect([$this->first_name, $this->middle_names, $this->last_name])
+            ->filter()
+            ->implode(' ');
+    }
+    //=======
+    //</ACCESSORS>
+    //=======
 }
