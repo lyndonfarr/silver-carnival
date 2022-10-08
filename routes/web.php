845c75d6504e838ddde6090dd2261a45e5e095da
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-08 12:34:07 -0300

    slim down routes. remove unnecessary

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::resource('/addresses', 'AddressController');
 Route::resource('/contacts', 'ContactController');
 Route::resource('/contact-extras', 'ContactExtraController');
-
-// Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
