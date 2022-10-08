291ad17ae7c476a78bc6bc4a1bd8d9e958af8a6d
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-08 12:53:31 -0300

    simplify ContactExtra:edit functionality

--- app/Http/Controllers/ContactExtraController.php
+++ app/Http/Controllers/ContactExtraController.php
@@ -21,13 +21,9 @@ class ContactExtraController extends Controller
     public function edit(Request $request, int $id): View
     {
         $contactExtra = ContactExtra::findOrFail($id);
-        
-        //@TODO retrieve the ContactExtras WITH the Contact. Wasn't working.
         $contact = Contact::findOrFail($contactExtra->contact_id);
-        
-        $contactExtras = ContactExtra::where(['contact_id' => $contact->id])->get()->groupBy('type');
 
-        return view('contact_extra.edit')->with(compact('contact', 'contactExtras', 'id'));
+        return view('contact_extra.edit')->with(compact('contact', 'contactExtra'));
     }
 
     /**
--- resources/views/contact_extra/edit.blade.php
+++ resources/views/contact_extra/edit.blade.php
@@ -1,50 +1,39 @@
 @extends('layouts.contact')
 
 @section('contactContent')
-<div class="card mb-4">
-    <div class="card-header d-flex justify-content-between align-items-center">
-        <ul class="nav nav-pills card-header-pills d-flex ml-auto">
-            <li class="nav-item">
-                <edit-button route="{{ route('contacts.edit', $contact->id) }}"></edit-button>
-            </li>
-        </ul>
-    </div>
-    <contact-show :contact="{{ $contact }}"></contact-show>
-</div>
-
-<div class="card">
-    <ul class="list-group list-group-flush">
-    @foreach ($contact->contactExtras as $contactExtra)
-        @if($contactExtra->id !== $id)
-            <contact-extra-list-item
-                :contact-extra="{{ $contactExtra }}"
-                edit-route="{{ route('contact-extras.edit', $contactExtra->id) }}"
-            ></contact-extra-list-item>
-        @else
-            <div class="list-group-item">
-                <form method="POST" action="{{ route('contact-extras.update', $contactExtra->id) }}" class="form-inline">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="contactExtra">{{ Str::ucfirst($contactExtra->type) }}</label>
-                        <input
-                            class="form-control mx-sm-3"
-                            id="contactExtra"
-                            name="value"
-                            type="string"
-                            value="{{ $contactExtra->value }}"
-                        >
-                        <button
-                            type="submit"
-                            class="btn btn-primary"
-                        >
-                            Save
-                        </button>
-                    </div>
-                </form>
+<form method="POST" action="{{ route('contact-extras.update', $contactExtra->id) }}">
+    @csrf
+    @method('PUT')
+    <div class="card mb-4">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <span>{{ $contact->full_name }}</span>
+            <ul class="nav nav-pills card-header-pills d-flex ml-auto">
+                <li class="nav-item">
+                    <edit-button route="{{ route('contacts.edit', $contact->id) }}"></edit-button>
+                </li>
+                <li class="nav-item">
+                    <show-button route="{{ route('contacts.show', $contact->id) }}"></show-button>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="form-row">
+                <text-input
+                    class="col-md-12"
+                    label="{{ Str::ucfirst($contactExtra->type) }}"
+                    name="value"
+                    value="{{ $contactExtra->value }}"
+                ></text-input>
             </div>
-        @endif
-    @endforeach
-    </ul>
-</div>
+        </div>
+        <div class="card-footer">
+            <button
+                type="submit"
+                class="btn btn-primary btn-block"
+            >
+                Save
+            </button>
+        </div>
+    </div>
+</form>
 @endsection
