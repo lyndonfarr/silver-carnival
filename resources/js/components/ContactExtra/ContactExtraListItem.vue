5100ca3007ecf7068c4a566f0f42b99725e82937
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-12 18:57:40 -0300

    remove edit logic from ContactExtraListItem vue component

--- resources/js/components/ContactExtra/ContactExtraListItem.vue
+++ resources/js/components/ContactExtra/ContactExtraListItem.vue
@@ -1,29 +1,19 @@
 <template>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>{{ contactExtra.type | ucfirst }}: {{ contactExtra.value }}</span>
-        <edit-button :route="editRoute"></edit-button>
+    <li class="list-group-item">
+        {{ contactExtra.type | ucfirst }}: {{ contactExtra.value }}
     </li>    
 </template>
 
 <script>
 import {ucfirst} from '../../filters';
-import EditButton from '../Buttons/Crud/EditButton.vue';
 
 export default {
     name: 'ContactExtraListItem',
-
-    components: {
-        EditButton,
-    },
 
     props: {
         contactExtra: {
             required: true,
             type: Object,
-        },
-        editRoute: {
-            required: true,
-            type: String,
         },
     },
 
