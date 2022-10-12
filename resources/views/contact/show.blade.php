6124f442b54e9a7afbb086246607a311a00f8618
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-12 18:58:41 -0300

    update contact.show blade file so that it can now be used to update the contact

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -1,25 +1,42 @@
 @extends('layouts.contact')
 
 @section('contactContent')
-<div class="card mb-4">
-    <div class="card-header d-flex justify-content-between align-items-center">
-        <ul class="nav nav-pills card-header-pills d-flex ml-auto">
-            <li class="nav-item">
-                <edit-button route="{{ route('contacts.edit', $contact->id) }}"></edit-button>
-            </li>
-        </ul>
-    </div>
-    <contact-show :contact="{{ $contact }}"></contact-show>
-</div>
+<value-storage :default-stored-value="{contact: {{ $contact }}}" inline-template>
+    <div class="card mb-4">
+        <div
+            class="card-header"
+            v-text="storedValue.contact.full_name"
+        ></div>
+            <ul class="list-group list-group-flush">
+                <contact-field-list-item
+                    :contact-id="storedValue.contact.id"
+                    field-name="full_name"
+                    label="Name"
+                    v-model="storedValue.contact.full_name"
+                ></contact-field-list-item>
+                <contact-field-list-item
+                    :contact-id="storedValue.contact.id"
+                    field-name="nickname"
+                    label="AKA"
+                    v-model="storedValue.contact.nickname"
+                ></contact-field-list-item>
+                @if ($contact->nickname)
+                    <li class="list-group-item">AKA: {{ $contact->nickname }}</li>
+                @endif
+                @if ($contact->dob)
+                    <li class="list-group-item">Age: {{ $contact->age }}</li>
+                @endif
+                <li class="list-group-item">Born: {{ $contact->dob }}</li>
+                <li class="list-group-item">Nationality: {{ $contact->nationality }}</li>
+                <li class="list-group-item">Notes: {{ $contact->notes }}</li>
 
-<div class="card">
-    <ul class="list-group list-group-flush">
-    @foreach ($contact->contactExtras as $contactExtra)
-        <contact-extra-list-item
-            :contact-extra="{{ $contactExtra }}"
-            edit-route="{{ route('contact-extras.edit', $contactExtra->id) }}"
-        ></contact-extra-list-item>
-    @endforeach
-    </ul>
-</div>
+                @foreach ($contact->contactExtras as $contactExtra)
+                    <contact-extra-list-item
+                        :contact-extra="{{ $contactExtra }}"
+                        edit-route="{{ route('contact-extras.edit', $contactExtra->id) }}"
+                    ></contact-extra-list-item>
+                @endforeach
+            </ul>
+    </div>
+</value-storage>
 @endsection
