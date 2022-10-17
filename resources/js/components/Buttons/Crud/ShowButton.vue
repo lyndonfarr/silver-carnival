31de13024ecbbfbb6ecfd595de90e7157b564cd3
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-17 08:05:15 -0300

    update edit and show buttons to use new icon components

--- resources/js/components/Buttons/Crud/ShowButton.vue
+++ resources/js/components/Buttons/Crud/ShowButton.vue
@@ -1,25 +1,18 @@
 <template>
     <a :href="route" title="view">
-        <svg
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            stroke="currentColor"
-            stroke-width="2"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="css-i6dzq1"
-        >
-            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-            <circle cx="12" cy="12" r="3"></circle>
-        </svg>
+        <show-icon></show-icon>
     </a>    
 </template>
 
 <script>
+import ShowIcon from '../../Icons/ShowIcon.vue';
+
 export default {
     name: 'ShowButton',
+
+    components: {
+        ShowIcon,
+    },
 
     props: {
         route: {
