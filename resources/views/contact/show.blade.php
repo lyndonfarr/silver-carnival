6aee4a56235180ecda339be5706e792541594455
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-17 08:07:55 -0300

    contact/show.blade: change look

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -1,42 +1,28 @@
 @extends('layouts.contact')
 
 @section('contactContent')
-<value-storage :default-stored-value="{contact: {{ $contact }}}" inline-template>
-    <div class="card mb-4">
-        <div
-            class="card-header"
-            v-text="storedValue.contact.full_name"
-        ></div>
-            <ul class="list-group list-group-flush">
-                <contact-field-list-item
-                    :contact-id="storedValue.contact.id"
-                    field-name="full_name"
-                    label="Name"
-                    v-model="storedValue.contact.full_name"
-                ></contact-field-list-item>
-                <contact-field-list-item
-                    :contact-id="storedValue.contact.id"
-                    field-name="nickname"
-                    label="AKA"
-                    v-model="storedValue.contact.nickname"
-                ></contact-field-list-item>
-                @if ($contact->nickname)
-                    <li class="list-group-item">AKA: {{ $contact->nickname }}</li>
-                @endif
-                @if ($contact->dob)
-                    <li class="list-group-item">Age: {{ $contact->age }}</li>
-                @endif
-                <li class="list-group-item">Born: {{ $contact->dob }}</li>
-                <li class="list-group-item">Nationality: {{ $contact->nationality }}</li>
-                <li class="list-group-item">Notes: {{ $contact->notes }}</li>
-
-                @foreach ($contact->contactExtras as $contactExtra)
-                    <contact-extra-list-item
-                        :contact-extra="{{ $contactExtra }}"
-                        edit-route="{{ route('contact-extras.edit', $contactExtra->id) }}"
-                    ></contact-extra-list-item>
-                @endforeach
-            </ul>
+<div class="card my-4">
+    <div class="card-header d-flex">
+        Show Contact
+        <edit-button
+            class="ml-auto"
+            route="{{ route('contacts.edit', $contact->id) }}"
+        ></edit-button>
     </div>
-</value-storage>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Name: {{ $contact->full_name }}</li>
+        <li class="list-group-item">AKA: {{ $contact->nickname }}</li>
+        <li class="list-group-item">Age: {{ $contact->age }}</li>
+        <li class="list-group-item">Born: {{ $contact->dob }}</li>
+        <li class="list-group-item">Nationality: {{ $contact->nationality }}</li>
+        <li class="list-group-item">Notes: {{ $contact->notes }}</li>
+    </ul>
+</div>
+<div class="card">
+    <ul class="list-group list-group-flush">
+        @foreach ($contact->contactExtras as $contactExtra)
+            <li class="list-group-item">{{ ucfirst($contactExtra->type) }}: {{ $contactExtra->value }}</li>
+        @endforeach
+    </ul>
+</div>
 @endsection
