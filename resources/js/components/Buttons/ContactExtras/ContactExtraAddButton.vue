f65047c15226a5c4a0de9aa16af9c5ff01802440
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-17 08:09:56 -0300

    ContactExtras/ContactExtraAddButton component: create component

---
+++ resources/js/components/Buttons/ContactExtras/ContactExtraAddButton.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="dropdown ml-auto">
+        <a
+            aria-expanded="false"
+            aria-haspopup="true"
+            class="text-success"
+            data-toggle="dropdown"
+            href=""
+            id="myDropdownId"
+            title="Add"
+            type="button"
+        >
+            <add-icon></add-icon>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button
+                class="dropdown-item"
+                @click="addContactExtra"
+                :key="contactExtraType"
+                :value="contactExtraType"
+                v-for="contactExtraType in contactExtraTypes"
+            >
+                {{ contactExtraType | ucfirst }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ContactExtraTypes} from '../../../config';
+import {ucfirst} from '../../../filters';
+import AddIcon from '../../Icons/AddIcon.vue';
+
+export default {
+    name: 'ContactExtraAddButton',
+
+    components: {
+        AddIcon,
+    },
+
+    filters: {
+        ucfirst,
+    },
+
+    data() {
+        return {
+            contactExtraTypes: ContactExtraTypes,
+        }
+    },
+
+    methods: {
+        addContactExtra(e) {
+            this.$emit('added', e.target.value);
+        },
+    },
+}
+</script>
