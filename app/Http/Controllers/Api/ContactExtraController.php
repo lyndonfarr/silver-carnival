f16b854f07f4a5e9baa468f86b5dbd940fb1c124
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-18 20:25:40 -0300

    new components: DestroyIcon and ApiDestroyButton

--- app/Http/Controllers/Api/ContactExtraController.php
+++ app/Http/Controllers/Api/ContactExtraController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\ContactExtra;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ContactExtraController extends Controller
 {
-    //
+    /**
+     * 
+     */
+    public function destroy(Request $request, int $id)
+    {
+        return ContactExtra::destroy($id);
+    }
 }
