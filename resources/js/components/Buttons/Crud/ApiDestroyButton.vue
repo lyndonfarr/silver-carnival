9ba130eed8f7eda665a01bcc2a35a42b9e536616
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-18 20:25:58 -0300

    new components: DestroyIcon and ApiDestroyButton

---
+++ resources/js/components/Buttons/Crud/ApiDestroyButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <a
+        class="d-flex align-items-center text-danger"
+        @click="destroy"
+        style="cursor: pointer;"
+        title="Destroy"
+    >
+        <destroy-icon></destroy-icon>
+    </a>
+</template>
+
+<script>
+import DestroyIcon from '../../Icons/DestroyIcon.vue';
+export default {
+    name: 'DestroyButton',
+
+    components: {
+        DestroyIcon,
+    },
+
+    props: {
+        endpoint: {
+            required: true,
+            type: String,
+        },
+    },
+
+    methods: {
+        destroy() {
+            axios
+                .delete(this.endpoint)
+                .then(res => this.$emit('deleted'))
+                .catch(err => console.log(err))
+            ;
+        },
+    },
+}
+</script>
