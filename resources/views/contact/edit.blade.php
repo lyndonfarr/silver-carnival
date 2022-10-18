e92bb450c5d11d96b5f6c9bd266b8d860ab79a58
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-18 20:27:37 -0300

    destroy buttons on ContactExtras on Contact.edit blade

--- resources/views/contact/edit.blade.php
+++ resources/views/contact/edit.blade.php
@@ -49,7 +49,7 @@
             <div class="card-header d-flex">
                 Edit ContactExtras
                 <contact-extra-add-button
-                    @input="e => storedValue.newContactExtras.push({type: e, value: ''})"
+                    @input="e => storedValue.newContactExtras = [...storedValue.newContactExtras, {type: e, value: '', key: storedValue.newContactExtras.length}]"
                 ></contact-extra-add-button>
             </div>
             <div class="card-body">
@@ -59,7 +59,31 @@
                     :name="`contact_extras[${contactExtra.id}][value]`"
                     v-for="(contactExtra, index) in storedValue.contact.contact_extras"
                     v-model="storedValue.contact.contact_extras[index].value"
-                ></text-input>
+                >
+                    <template v-slot:buttons>
+                        <api-destroy-button
+                            class="ml-2"
+                            :endpoint="`/api/contact-extras/${contactExtra.id}`"
+                        ></api-destroy-button>
+                    </template>
+                </text-input>
+                <text-input
+                    :key="`new-contact-extra-${newContactExtra.key}`"
+                    :label="newContactExtra.type"
+                    :name="`contact_extras[${newContactExtra.key}][value]`"
+                    v-for="(newContactExtra, index) in storedValue.newContactExtras"
+                    v-model="storedValue.newContactExtras[index].value"
+                >
+                    <template v-slot:buttons>
+                        <a
+                            class="d-flex align-items-center text-danger ml-2"
+                            @click="e => storedValue.newContactExtras = [...storedValue.newContactExtras.slice(0, index), ...storedValue.newContactExtras.slice(index + 1, storedValue.newContactExtras.length)]"
+                            style="cursor: pointer;"
+                        >
+                            <destroy-icon></destroy-icon>
+                        </a>
+                    </template>
+                </text-input>
             </div>
         </div>
 
