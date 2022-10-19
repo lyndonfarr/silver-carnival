d3b229f99f159c22f6252dbeebdd13f01145b9c2
Lyndon Farr <lyndonfarr@protonmail.com>
2022-10-19 20:08:00 -0300

    Contact.Update request validation

--- app/Http/Requests/Contact/Update.php
+++ app/Http/Requests/Contact/Update.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Contact;
 
+use App\ContactExtra;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class Update extends FormRequest
@@ -11,7 +13,7 @@ class Update extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,14 +23,34 @@ class Update extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
-        return [
+        $rules = [];
+
+        $contactExtraTypeRule = Rule::in([ContactExtra::TYPE_INSTAGRAM, ContactExtra::TYPE_PHONE]);
+        $contactExtraValueRule = 'required|max:255';
+
+        if (isset($this->new_contact_extras)) {
+            foreach ($this->new_contact_extras as $index => $newContactExtra) {
+                $rules["new_contact_extras.{$index}.type"] = $contactExtraTypeRule;
+                $rules["new_contact_extras.{$index}.value"] = $contactExtraValueRule;
+            }
+        }
+        
+        if (isset($this->contact_extras)) {
+            foreach ($this->contact_extras as $index => $contactExtra) {
+                $rules["contact_extras.{$index}.id"] = 'integer|exists:contact_extras';
+                $rules["contact_extras.{$index}.type"] = $contactExtraTypeRule;
+                $rules["contact_extras.{$index}.value"] = $contactExtraValueRule;
+            }
+        }
+
+        return array_merge($rules, [
             'dob' => 'date_format:Y-m-d|before:now|nullable',
             'full_name' => 'required|max:255',
             'nationality' => 'max:255',
             'nickname' => 'max:255',
             'notes' => 'max:4095',
-        ];
+        ]);
     }
 }
