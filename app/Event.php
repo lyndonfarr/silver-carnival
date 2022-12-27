c7a5d49ea9af52510221302e57b75893d2a26a6d
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-27 19:29:11 -0300

    map date fields in Event.php

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'deleted_at',
+        'date',
+        'updated_at',
+    ];
 }
