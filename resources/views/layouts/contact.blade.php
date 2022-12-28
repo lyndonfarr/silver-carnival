c7ed79401ee2041e597d5a1d208d3bcd1ece67d9
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-28 16:54:49 -0300

    change order of links on subnav

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -5,8 +5,8 @@
 <nav class="nav nav-pills mb-4 justify-content-center">
     <a class="nav-link {{ Route::is('contacts.index') ? 'active' : '' }}" href="{{ route('contacts.index') }}">Index</a>
     <a class="nav-link {{ Route::is('contacts.create') ? 'active' : '' }}" href="{{ route('contacts.create') }}">Create</a>
-    <a class="nav-link disabled {{ Route::is(['contacts.edit', 'contact-extras.edit']) ? 'active' : '' }}" href="#">Edit</a>
     <a class="nav-link disabled {{ Route::is('contacts.show') ? 'active' : '' }}" href="#">Show</a>
+    <a class="nav-link disabled {{ Route::is(['contacts.edit', 'contact-extras.edit']) ? 'active' : '' }}" href="#">Edit</a>
 </nav>
 
 @yield('contactContent')
--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -5,8 +5,8 @@
 <nav class="nav nav-pills mb-4 justify-content-center">
     <a class="nav-link {{ Route::is('events.index') ? 'active' : '' }}" href="{{ route('events.index') }}">Index</a>
     <a class="nav-link {{ Route::is('events.create') ? 'active' : '' }}" href="{{ route('events.create') }}">Create</a>
-    <a class="nav-link disabled {{ Route::is('events.edit') ? 'active' : '' }}" href="#">Edit</a>
     <a class="nav-link disabled {{ Route::is('events.show') ? 'active' : '' }}" href="#">Show</a>
+    <a class="nav-link disabled {{ Route::is('events.edit') ? 'active' : '' }}" href="#">Edit</a>
 </nav>
 
 @yield('eventContent')
