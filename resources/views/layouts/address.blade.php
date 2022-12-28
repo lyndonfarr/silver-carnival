6423abd5d00d72851e8d45cfc6b6035b3575cb05
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-28 16:58:24 -0300

    remove Addresses::index link

--- resources/views/layouts/address.blade.php
+++ resources/views/layouts/address.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <nav class="nav nav-pills mb-4 justify-content-center">
-    <a class="nav-link {{ Route::is('addresses.index') ? 'active' : '' }}" href="{{ route('addresses.index') }}">Index</a>
     <a class="nav-link {{ Route::is('addresses.create') ? 'active' : '' }}" href="{{ route('addresses.create') }}">Create</a>
     <a class="nav-link disabled {{ Route::is('addresses.show') ? 'active' : '' }}" href="#">Show</a>
     <a class="nav-link disabled {{ Route::is('addresses.edit') ? 'active' : '' }}" href="#">Edit</a>
