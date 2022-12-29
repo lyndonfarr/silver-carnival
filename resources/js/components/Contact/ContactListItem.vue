93d5a999c7216c8901acd682f16199646f159fbb
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-29 18:42:07 -0300

    change styling on contacts/index links

--- resources/js/components/Contact/ContactListItem.vue
+++ resources/js/components/Contact/ContactListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    <a :href="showRoute" v-text="contact.full_name"></a>
+    <a class="text-dark" :href="showRoute" v-text="contact.full_name"></a>
     <span>
         <whats-app-button
             :phone-number="contact.primary_phone_number.value"
