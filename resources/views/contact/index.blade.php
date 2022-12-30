c0d531d73cc33547156439e777e58b5b61cc6f96
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-30 12:03:53 -0300

    update contacts::index page look

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -2,13 +2,16 @@
 
 @section('contactContent')
 
-<ul class="list-group">
-    @foreach($contacts as $contact)
-        <contact-list-item
-            :contact="{{ $contact }}"
-            show-route="{{ route('contacts.show', $contact->id) }}"
-        ></contact-list-item>
-    @endforeach
-</ul>
+<div class="card">
+    <div class="card-header">Index Contacts</div>
+    <ul class="list-group list-group-flush">
+        @foreach($contacts as $contact)
+            <contact-list-item
+                :contact="{{ $contact }}"
+                show-route="{{ route('contacts.show', $contact->id) }}"
+            ></contact-list-item>
+        @endforeach
+    </ul>
+</div>
 
 @endsection
