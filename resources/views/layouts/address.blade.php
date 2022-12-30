8a5e158f364bd2c015b82d92e0bcd7a3be655c63
Lyndon Farr <lyndonfarr@protonmail.com>
2022-12-30 17:48:58 -0300

    align sub navs to the left, add search box to events::index page

--- resources/views/layouts/address.blade.php
+++ resources/views/layouts/address.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<nav class="nav nav-pills mb-4 justify-content-center">
+<nav class="nav nav-pills mb-4">
     <bs-nav-pill class="{{ Route::is('addresses.show') ? 'active' : '' }}" href="{{ route('addresses.show', $address->id) }}">Show</bs-nav-pill>
     <bs-nav-pill class="{{ Route::is('addresses.edit') ? 'active' : '' }}" href="{{ route('addresses.edit', $address->id) }}">Edit</bs-nav-pill>
 </nav>
--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<nav class="nav nav-pills mb-4 justify-content-center">
+<nav class="nav nav-pills mb-4">
     <bs-nav-pill class="{{ Route::is('events.index') ? 'active' : '' }}" href="{{ route('events.index') }}">Index</bs-nav-pill>
     <bs-nav-pill class="{{ Route::is('events.create') ? 'active' : '' }}" href="{{ route('events.create') }}">Create</bs-nav-pill>
     @if (Route::is(['events.edit', 'events.show']))
@@ -11,6 +11,16 @@
     @else
         <bs-nav-pill class="disabled" href="#">Show</bs-nav-pill>
         <bs-nav-pill class="disabled" href="#">Edit</bs-nav-pill>
+    @endif
+
+    @if (Route::is(['events.index']))
+        <form class="form-inline ml-auto">
+            <div class="form-group mx-sm-3">
+                <label for="search" class="sr-only">Search</label>
+                <input type="text" class="form-control" id="search" placeholder="Search...">
+            </div>
+            <button type="submit" class="btn btn-dark">Search</button>
+        </form>
     @endif
 </nav>
 
