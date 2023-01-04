5075fc2f350ccb6423ffad3c13c588a787ef3d88
Lyndon Farr <lyndonfarr@protonmail.com>
2023-01-04 20:32:00 -0300

    new relationships on Event for recurring events. new method to get next date for recurrence (needs moving). new method to create recurring events.

--- app/Event.php
+++ app/Event.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -53,6 +54,26 @@ class Event extends Model
     {
         return $this->belongsTo(Contact::class);
     }
+
+    /**
+     * An Event BelongsTo an InitialEvent
+     * 
+     * @return BelongsTo
+     */
+    public function initialEvent(): BelongsTo
+    {
+        return $this->belongsTo(self::class);
+    }
+
+    /**
+     * An Event HasMany RecurredEvents
+     * 
+     * @return HasMany
+     */
+    public function recurredEvents(): HasMany
+    {
+        return $this->HasMany(self::class, 'recurred_event_id');
+    }
     
     /**
      * An Event BelongsTo a RememberedContact
@@ -86,14 +107,73 @@ class Event extends Model
     }
 
     /**
+     * Get the next date for a recurring event, which the date provided may be in the past i.e. DoB for a Contact
+     * 
+     * @todo where to move this method to. Does not belong in Event model as it has nothing to do with Events.
+     * 
+     * @param Carbon $date The date Event first took place.
+     * @param string $recurrence The period to recur the Event at.
+     * @return Carbon
+     */
+    public static function getNextDateForRecurrence(Carbon $date, string $recurrence): Carbon
+    {
+        if (!$date->isPast()) {
+            return $date;
+        }
+
+        $now = Carbon::now();
+        $secondsSinceMidnight = $now->secondsSinceMidnight();
+        $startOfToday = $now->subSeconds($secondsSinceMidnight);//If the first Event is today, even if it was earlier in the day, store it in DB.
+
+        if ($recurrence === self::RECURRENCE_TYPE_YEARLY) {
+            $diffYears = $date->diffInYears($startOfToday);
+            return $date->addYears($diffYears + 1);
+        }
+
+        if ($recurrence === self::RECURRENCE_TYPE_MONTHLY) {
+            $diffMonths = $date->diffInMonths($startOfToday);
+            return $date->addMonths($diffMonths + 1);
+        }
+
+        if ($recurrence === self::RECURRENCE_TYPE_WEEKLY) {
+            $diffWeeks = $date->diffInWeeks($startOfToday);
+            return $date->addWeeks($diffWeeks + 1);
+        }
+
+        if ($recurrence === self::RECURRENCE_TYPE_DAILY) {
+            $diffDays = $date->diffInDays($startOfToday);
+            return $date->addDays($diffDays + 1);
+        }
+
+        /**
+         * @todo throw an exception, log an error.
+         */
+        return $date;
+    }
+
+    /**
      * Create a recurring Event
      * 
-     * @param array $data The Event data that will be used for all recurred Events.
-     * @param string $recurrence How regularly the Event will be recur.
+     * @param array $eventData The initial Event data.
+     * @param string $recurrence How regularly the Event will recur.
      * @param Carbon $untilDate When to stop recurring the Event.
+     * @return bool
      */
-    public static function createRecurring(array $data, string $recurrence, Carbon $untilDate)
+    public static function createRecurring(array $eventData, string $recurrence, Carbon $untilDate): bool
     {
+        $initialEvent = self::create($eventData);
+        $eventData['recurred_event_id'] = $initialEvent->id;
+
         $events = [];
+        $date = $initialEvent->date->copy();
+        
+        while ($date <= $untilDate) {
+            $date->add(1 . ' ' . config("mappings.recurrence_period_to_carbon.{$recurrence}"));
+            $eventData['date'] = $date;
+            $events[] = $eventData;
+            $date = $date->copy();
+        }
+
+        return self::insert($events);
     }
 }
